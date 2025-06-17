7e564ca42663543186b50a4fe16f36566560c3f0
rjangid631 <rohanjangid631@gmail.com>
2025-06-17 16:08:35 +0530

    Last and final updated code

--- clients/Serializersclient/testcasedata.py
+++ clients/Serializersclient/testcasedata.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from clients.models.testdata import TestData
 
 class TestCaseDataSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.name', read_only=True)  # Optional: for display
+
     class Meta:
         model = TestData
-        fields = ['company_id', 'service_name', 'case_per_day', 'number_of_days', 'total_case']
+        fields = [
+            'id',
+            'client',         # updated from 'company_id'
+            'client_name',    # optional, for display in API
+            'service_name',
+            'case_per_day',
+            'number_of_days',
+            'total_case',
+        ]
